068f146739124df73c7008ba1e0bafed7d50b878
Hashane <jhashane@gmail.com>
2025-11-22 05:47:41 +0300

    Implementation: Task #158 - Configure rate limiting - Install required packages - Socialite. - Setup CORS for mobile app

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,10 @@ namespace App\Providers;
 
 use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Cache\RateLimiting\Limit;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\RateLimiter;
+
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -23,5 +27,9 @@ class AppServiceProvider extends ServiceProvider
         if (config('app.env') === 'production') {
             URL::forceScheme('https');
         }
+
+        RateLimiter::for('api', function (Request $request) {
+            return Limit::perMinute(60)->by($request->user()?->id ?: $request->ip());
+        });
     }
 }
