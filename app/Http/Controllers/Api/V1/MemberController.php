ff2302e21497b4fadf3521c53969267627799912
Hashane <jhashane@gmail.com>
2025-11-22 23:16:42 +0300

    Refactor: Moved directory.

--- app/Http/Controllers/Api/V1/MemberController.php
+++ app/Http/Controllers/Api/V1/MemberController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers\Api\V1;
 
 use App\Enums\VerificationCodeType;
 use App\Http\Controllers\Controller;
-use App\Http\Requests\UpdateProfileRequest;
+use App\Http\Requests\Api\V1\UpdateProfileRequest;
 use App\Http\Requests\VerifyEmailRequest;
 use App\Http\Resources\Api\V1\MemberResource;
 use App\Mail\EmailVerificationMail;
