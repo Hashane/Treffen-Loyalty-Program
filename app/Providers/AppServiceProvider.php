c4732dce798fbd337b643df47b63fcce68744f96
Hashane <jhashane@gmail.com>
2025-11-22 10:20:14 +0300

    Enhancement: Response Macro created.

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,12 +2,12 @@
 
 namespace App\Providers;
 
-use Illuminate\Support\Facades\URL;
-use Illuminate\Support\ServiceProvider;
 use Illuminate\Cache\RateLimiting\Limit;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\RateLimiter;
-
+use Illuminate\Support\Facades\URL;
+use Illuminate\Support\ServiceProvider;
+use Response;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -30,6 +30,21 @@ class AppServiceProvider extends ServiceProvider
 
         RateLimiter::for('api', function (Request $request) {
             return Limit::perMinute(60)->by($request->user()?->id ?: $request->ip());
+        });
+
+        Response::macro('success', function ($data = null, $message = 'Success', $code = 200) {
+            return response()->json([
+                'success' => true,
+                'message' => $message,
+                'data' => $data,
+            ], $code);
+        });
+
+        Response::macro('error', function ($message, $code = 400) {
+            return response()->json([
+                'success' => false,
+                'message' => $message,
+            ], $code);
         });
     }
 }
